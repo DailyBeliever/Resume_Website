2bbac07a576d51458197ad871080729f11c1d73f
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-02-06 22:14:50 -0800

    Update

--- resume_website/src/app/components/HeroSection.tsx
+++ resume_website/src/app/components/HeroSection.tsx
@@ -1,10 +1,24 @@
+// Components: Reuseable code for pages that can be 
+// imported on any page to keep the same design
 import React from "react";
 
 const HeroSection = () => {
     return (
         <section>
-            <h1 className= "text-white"> Portfolio Website </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7">
+                <h1 className= "text-[#ffffff] mb-4 text-4xl lg:text-6xl font-extrabold"> 
+                Hello, I'm Joshua! 
+                </h1>
+                <p className="text-[#ABB7BE] text-lg lg:text-xl"> 
+                This is were my intro text would go!
+                .
+                .
+                </p>
+            </div>
+        </div>
         </section>
-
     );
 };
+
+export default HeroSection;
--- resume_website/src/app/page.tsx
+++ resume_website/src/app/page.tsx
@@ -1,9 +1,10 @@
-
-
+// This is the import from the components folder
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-white">
+    <main className="flex min-h-screen flex-col bg-[#292828] containter mx-auto px-12 py-12">
+      <HeroSection />
     </main>
   );
 }
