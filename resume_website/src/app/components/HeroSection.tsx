9c24172bdf086bd0d89a41f264ee0bfc3ea500d1
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-02-06 21:36:03 -0800

    Update

---
+++ resume_website/src/app/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1 className= "text-white"> Portfolio Website </h1>
+        </section>
+
+    );
+};
---
+++ resume_website/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-white">
+    </main>
+  );
+}
