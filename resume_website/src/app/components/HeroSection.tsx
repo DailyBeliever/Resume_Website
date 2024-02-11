a2c5749482a23d3fddf5f8974a91dc21aa4a8930
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-02-10 22:16:12 -0800

    Update HeroSection.tsx

--- resume_website/src/app/components/HeroSection.tsx
+++ resume_website/src/app/components/HeroSection.tsx
@@ -9,14 +9,19 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place self-center text-center sm:text-left">
                 <h1 className= "text-[#ffffff] mb-4 text-4xl lg:text-6xl font-extrabold"> 
-                Hello, I'm Joshua! 
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{" "}</span> 
+                    Joshua!
                 </h1>
                 <p className="text-[#ABB7BE]text-base sm:text text-lg mb-6 lg:text-xl"> 
                 Welcome To My Resume Website!
                 </p>
                 <div>
-                    <button className= "px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 via-blue-500 to-red-500 hover:bg-slate-200 text-white font-bold"> My Resume </button>
-                    <button className= "px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 via-blue-500 to-red-500 hover:bg-slate-200 text-white font-bold"> Terminal </button>
+                    <button className= "px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 via-blue-500 to-red-500 hover:bg-slate-200 text-white font-bold">
+                        <span className= "block bg-[#292828] hover:bg-slate-800 rounded-full px-5 py-2 "> My Resume </span>
+                    </button>
+                    <button className= "px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 via-blue-500 to-red-500 hover:bg-slate-200 text-white font-bold"> Terminal 
+                    
+                    </button>
                 </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 mb-6 lg:mt-0">
