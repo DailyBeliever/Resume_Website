c456b1493317df849640db39c7d92de9ca635a8e
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-03-04 21:50:17 -0800

    Update background.tsx

---
+++ src/components/background.tsx
@@ -0,0 +1,9 @@
+// This is the import from the components folder
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#292828] containter mx-auto px-12 py-4">
+    </main>
+  );
+}
