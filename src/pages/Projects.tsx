005dc92b33190d9fd35d15e9bda638b92e5ad965
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-03-26 21:17:53 -0700

    Update Projects.tsx

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const  Projects = () => {
+    return <section className="text-white">
+
+    <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">    
+    
+    </div>About Section
+    
+    </section>
+};
+
+export default Projects
