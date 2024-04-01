c9baf2766a7bfaaf76b01ea7fda09e3bfab848ea
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-03-31 20:40:31 -0700

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,6 +5,7 @@ const  AboutSection = () => {
 
     <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">    
     
+    
     </div>About Section
     
     </section>
