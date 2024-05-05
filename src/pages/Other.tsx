7fcb16665400d7fe492e70087101a54ee7750302
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-05-04 23:32:23 -0700

    Update Other.tsx

--- src/pages/Other.tsx
+++ src/pages/Other.tsx
@@ -4,7 +4,11 @@ const  Other = () => {
     return <section className="text-white">
 
     <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">    
-    
+        <p>
+
+            This will be for other info
+
+        </p>
     
     </div>Contact
     
