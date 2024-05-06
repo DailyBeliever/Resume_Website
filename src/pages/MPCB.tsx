060456a212e06a08b0da55c3006a1e69f90e26cf
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-05-05 21:55:35 -0700

    Update MPCB.tsx

--- src/pages/MPCB.tsx
+++ src/pages/MPCB.tsx
@@ -4,7 +4,9 @@ const  MPCB = () => {
     return <section className="text-white">
 
     <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">    
-    
+    <p>
+        This will be for MPCB
+    </p>
     
     </div>MPCB 
     
