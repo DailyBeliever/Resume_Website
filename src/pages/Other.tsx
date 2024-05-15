227c56b2ffdfa20963b655c00ff0c8fcfcc7c152
Joshua Chavez <91343098+DailyBeliever@users.noreply.github.com>
2024-05-14 20:31:33 -0700

    Update Other.tsx

--- src/pages/Other.tsx
+++ src/pages/Other.tsx
@@ -6,6 +6,7 @@ const  Other = () => {
     <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">    
         <p>
             This will be for other info | Need to figure out what to put here
+            
         </p>
     
     </div>Contact
